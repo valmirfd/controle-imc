03a217543eec22a8f21cec2c4d025c813d65cf2e
valmirfd <fernandocursos2021@gmail.com>
2023-09-09 17:52:14 -0300

    Tela de resultado criada

--- app/src/main/java/com/valorizeseusamigos/controle_imc/MainActivity.kt
+++ app/src/main/java/com/valorizeseusamigos/controle_imc/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.valorizeseusamigos.controle_imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
